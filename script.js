6cf279615b72eaa67a1224840ec917354a2ac76a
sdiperi90 <sirgasheva17@gmail.com>
2018-09-15 11:13:33 -0400

    added shuffle feature

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
 const cards = document.querySelectorAll(".memory-card");
 
-let hasFlippedCard = false
+let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle("flip");
+    if (lockBoard) return;
+    if (this === firstCard) return;
 
-    if(!hasFlippedCard) {
+    this.classList.add("flip");
+
+    if (!hasFlippedCard) {
         // first click
         hasFlippedCard = true;
         firstCard = this;
 
-        console.log(hasFlippedCard, firstCard);
-    
-}
+        return;
+    }
+
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch();
 }
 
+function checkForMatch() {
+    let isMatch = firstCard.dataset.character === secondCard.dataset.character;
 
+    isMatch ? disableCards() : unflippedCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+
+    resetBoard();
+}
+
+function unflippedCards() {
+    localBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
 cards.forEach(card => card.addEventListener("click", flipCard));
