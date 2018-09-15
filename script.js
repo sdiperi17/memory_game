5f29de601816fa5181e3e866d2d7c3c0324fa935
sdiperi90 <sirgasheva17@gmail.com>
2018-09-15 01:38:10 -0400

    added data character attributes to every memory card

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const cards = document.querySelectorAll(".memory-card");
 
+let hasFlippedCard = false
+let firstCard, secondCard;
+
 function flipCard() {
     this.classList.toggle("flip");
+
+    if(!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+
+        console.log(hasFlippedCard, firstCard);
+    
 }
+}
+
 
 cards.forEach(card => card.addEventListener("click", flipCard));
